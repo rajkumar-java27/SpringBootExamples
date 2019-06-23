7e399dc9b3559700b909b270c8c4dd2ffdc5b82b
Rajkumar Chilukuri <Rajkumar.Chilukuri@global.internal>
2019-06-23 20:05:09 +0530

    sample Test code added

---
+++ ADPServices/src/main/java/info/ravetech/AdpServicesApplication.java
@@ -0,0 +1,13 @@
+package info.ravetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdpServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdpServicesApplication.class, args);
+	}
+
+}
